1ea6edef8161672c20a4f74fdb99380d63a3e144
01paulina <paulinalondonodiaz01@gmail.com>
2026-04-23 19:05:28 -0500

    se crea el codigo de cursos en la carga

--- carga.py
+++ carga.py
@@ -8,4 +8,14 @@ def cargar_datos(ruta_archivo):
         return df_datos
     except FileNotFoundError:
         print(f"\nError: No se encontró el archivo en {ruta_archivo}")
-        return None
+        return df_datos
+
+def cargar_cursos(ruta_archivo):
+    try:
+        
+        df_curso = pd.read_csv(ruta_archivo)
+        print("\n¡Archivo de cursos cargado con éxito!")
+        return df_curso # <--- Obligatorio
+    except Exception as e:
+        print(f"\nError al cargar cursos: {e}")
+        return df_curso
